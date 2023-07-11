4e7badc7c8c167be74313ef0039af4e0a48c2c10
Даниил <daniilspektoruk009@gmail.com>
2023-07-11 10:40:09 +0700

    improved version commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 
 from aiogram.filters import Command, Text
-from aiogram.types import Message
+from aiogram.types import Message, ContentType, Sticker, voice, video
 
 from config import config
 
@@ -27,9 +27,41 @@ async def lucky_command(message: Message):
     await message.answer('Вы открыли пасхалку!')
 
 
+#
+#
+# @dp.message(F.content_type == ContentType.PHOTO)
+# async def echo_photo(message: Message):
+#     await message.answer_photo(message.photo[0].file_id)
+#
+#
+# @dp.message(F.content_type == ContentType.STICKER)
+# async def echo_sticker(message: Message):
+#     await message.answer_sticker(message.sticker.file_id)
+#
+#
+# @dp.message(F.content_type == ContentType.VOICE)
+# async def echo_voice(message: Message):
+#     await message.answer_voice(message.voice.file_id)
+#
+#
+# @dp.message(F.content_type == ContentType.VIDEO)
+# async def echo_video(message: Message):
+#     await message.answer_video(message.video.file_id)
+#
+#
+# @dp.message()
+# async def echo(message: Message):
+#     await message.answer(message.text)
+
+
+@dp.message(Text(text='Ответь'))
+async def reply(message: Message):
+    await message.reply('Ответил')
+
+
 @dp.message()
-async def echo(message: Message):
-    await message.answer(message.text)
+async def echo_all(message: Message):
+    await message.send_copy(message.chat.id)
 
 
 async def main():
